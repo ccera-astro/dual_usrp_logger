6569a52bce11e5f0f6bee08b70e1376f26a1bf78
Marcus D. Leech <patchvonbraun@gmail.com>
2022-03-25 13:38:50 -0400

    Updated lmst_waitfor to use a narrow window.

--- lmst_wait.py
+++ lmst_wait.py
@@ -4,6 +4,7 @@ import time
 import sys
 import os
 import ephem
+import argparse
 
 # Given longitude(decimal degrees as a float)
 #
@@ -30,15 +31,21 @@ def cur_sidereal(longitude):
     sidt = "%02d,%02d,%02d" % (hours, minutes, seconds)
     return (sidt)
 
+TENSECS = 10.0/3600.0
+parser = argparse.ArgumentParser(description="Wait for a given LMST then exit")
+parser.add_argument("--longitude", type=float, help="Local longitude", required=True)
+parser.add_argument("--lmst", type=float, help="LMST to wait for", required=True)
+
+args = parser.parse_args()
 
 while True:
-    x = cur_sidereal(float(sys.argv[1]))
+    x = cur_sidereal(args.longitude)
     x = x.split(",")
     lmst = float(x[0])
     lmst += float(x[1])/60.0
     lmst += float(x[2])/3600.0
     
-    if (lmst >= float(sys.argv[2])):
+    if (lmst >= args.lmst and lmst <= (args.lmst + TENSECS)):
         break
 
     time.sleep(1.0)
